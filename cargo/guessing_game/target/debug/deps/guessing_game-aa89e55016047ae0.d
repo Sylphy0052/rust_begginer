b67d7e88548276d5a08e69bc661d766079820133
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-07-26 23:07:08 +0900

    delete: unuse files

--- cargo/guessing_game/target/debug/deps/guessing_game-aa89e55016047ae0.d
+++
@@ -1,5 +0,0 @@
-/Users/pyente/Github/rust_begginer/cargo/guessing_game/target/debug/deps/guessing_game-aa89e55016047ae0: src/main.rs
-
-/Users/pyente/Github/rust_begginer/cargo/guessing_game/target/debug/deps/guessing_game-aa89e55016047ae0.d: src/main.rs
-
-src/main.rs:
